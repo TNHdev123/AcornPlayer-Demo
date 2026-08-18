2462bde0edff86213a117c33112856246a20f7e6
TNHdev123 <TNHdev123@outlook.com>
2026-08-18 17:58:48 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "AcornPlayerDemo",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .executable(name: "AcornPlayerDemo", targets: ["AcornPlayerDemo"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "AcornPlayerDemo",
+            path: "AcornPlayerDemo"
+        )
+    ]
+)
